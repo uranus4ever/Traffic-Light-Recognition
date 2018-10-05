a14fcc3f74beb7d7d31d5dab10b6a4d385fe871f
Chaoqun Shan <shanchaoqun27@163.com>
2018-10-05 15:13:40 +0800

    Color Identifier
    Create an identifier to distinguish RED, YELLOW, and GREEN

--- color.py
+++ color.py
@@ -15,40 +15,42 @@ import matplotlib.image as mpimg
 pic_day_r = mpimg.imread('./image/daySequence1--00150.png')
 pic_day_g = mpimg.imread('./image/daySequence1--00222.png')
 pic_day_y = mpimg.imread('./image/daySequence1--01902.png')
-# pic_night = mpimg.imread('./image/nightClip4--00148.png')
+pic_night = mpimg.imread('./image/nightClip4--00148.png')
 
-# plt.figure()
-# plt.subplot(321)
-# plt.imshow(pic_day_r)
-# plt.subplot(322)
-# plt.imshow(pic_day_r[400:480, 780:820, :])
-# plt.subplot(323)
-# plt.imshow(pic_day_g)
-# plt.subplot(324)
-# plt.imshow(pic_day_g[380:460, 1000:1060, :])
-# plt.subplot(325)
-# plt.imshow(pic_day_y)
-# plt.subplot(326)
-# plt.imshow(pic_day_y[360:410, 720:750, :])
-# plt.show()
 
 day_r = pic_day_r[400:480, 780:820, :]
 day_g = pic_day_g[380:460, 1000:1060, :]
 day_y = pic_day_y[360:410, 720:750, :]
 
-def ColorIdentfier(pic):
+"""
+Through test, 
+A channel from LAB space can identify RED from GREEN and YELLOW. pixel thres is about 50.
+L channel from LUV space can identify YELLOW from RED and GREEN. pixel thres is 90.
+"""
+def ColorIdentifier(pic, a_thres=50, l_thres=90):
 
-    return
+    a_channel = cv2.cvtColor(pic, cv2.COLOR_RGB2Lab)[:, :, 1]
+    l_channel = cv2.cvtColor(pic, cv2.COLOR_RGB2LUV)[:, :, 0]
+
+    if (a_channel > a_thres).sum() > 30: # qualified pixel quantity
+        return "RED"
+    elif (l_channel > l_thres).sum() > 100: # qualified pixel quantity
+        return "YELLOW"
+    else:
+        return "GREEN"
 
 
-def TransferColor(origin, LAB="b"):
-    if LAB == "l":
+def TransferColor(origin, ch="a"):
+    if ch == "l_lab":
         out = cv2.cvtColor(origin, cv2.COLOR_RGB2Lab)[:, :, 0]
-    elif LAB == "a":
+    elif ch == "a":
         out = cv2.cvtColor(origin, cv2.COLOR_RGB2Lab)[:, :, 1]
-    elif LAB == "b":
+    elif ch == "b":
         out = cv2.cvtColor(origin, cv2.COLOR_RGB2Lab)[:, :, 2]
+    elif ch == "l_luv":
+        out = cv2.cvtColor(origin, cv2.COLOR_RGB2LUV)[:, :, 0]
     return out
+
 
 def PlotPic(pic):
     # pic contains 6 pictures
@@ -64,5 +66,8 @@ def PlotPic(pic):
     return 1
 
 if __name__ == "__main__":
-    p = PlotPic([pic_day_r, day_r, pic_day_g, day_g, pic_day_y, day_y])
-    print("OK")
+    p = PlotPic([TransferColor(day_r, 'a'), TransferColor(day_r, 'l_luv'),
+                 TransferColor(day_g, 'a'), TransferColor(day_g, 'l_luv'),
+                 TransferColor(day_y, 'a'), TransferColor(day_y, 'l_luv')])
+    print(ColorIdentifier(day_r), ColorIdentifier(day_g), ColorIdentifier(day_y))
+
